3e47d5c4343cb1bd4ef75f81bcb1b278cb046d60
Machu Rexford <[rexfordmachu@gmail.com]>
2020-11-24 23:26:56 +0000

    Initial Commit

---
+++ Shared/qodehub_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  qodehub_swiftuiApp.swift
+//  Shared
+//
+//  Created by Rexford Machu on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct qodehub_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
